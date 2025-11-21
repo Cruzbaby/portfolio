787985f412b888fafaa702ef8f6f8998330d7372
Cruzbaby <84595207+Cruzbaby@users.noreply.github.com>
2025-11-21 21:20:23 +0700

    Change Setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.cruz-dev.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "registry.npmmirror.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "mir-s3-cdn-cf.behance.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
